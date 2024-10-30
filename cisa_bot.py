f0ffe3320ebd751551c06b011b91b30c558820c6
Omer <136714975+ums91@users.noreply.github.com>
2024-10-30 15:12:04 +0530

    Update cisa_bot.py

--- cisa_bot.py
+++ cisa_bot.py
@@ -4,7 +4,7 @@ import time
 from github import Github, GithubException
 
 # GitHub and CISA credentials
-GITHUB_TOKEN = os.getenv("MY_TOKEN")
+GITHUB_TOKEN = os.getenv("NEW_TOKEN")
 CISA_API_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
 REPO_NAME = "ums91/CISA_BOT"  # Replace with your GitHub repository
 
