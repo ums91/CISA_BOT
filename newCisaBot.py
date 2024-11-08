2043b360f465879c886632b2b5c840d119882e88
Omer <136714975+ums91@users.noreply.github.com>
2024-11-08 14:10:13 +0530

    Update newCisaBot.py

--- newCisaBot.py
+++ newCisaBot.py
@@ -55,16 +55,6 @@ class Main:
     def __init__(self):
         self.github = Github(os.getenv("GITHUB_TOKEN"))
         self.repo = self.github.get_repo(Constants.GITHUB_REPO)
-        self.milestone = self.get_milestone()
-
-    def get_milestone(self):
-        """ Get the milestone object for "2024Q2" """
-        milestones = self.repo.get_milestones(state="open")
-        for milestone in milestones:
-            if milestone.title == Constants.MILESTONE_NAME:
-                return milestone
-        log_message(f"Milestone {Constants.MILESTONE_NAME} not found", "exiting")
-        sys.exit(os.EX_DATAERR)
 
     def download_cisa_list(self):
         """ download issues from CISA """
@@ -186,48 +176,54 @@ From CISA Known Exploited Vulnerabilities Catalog: https://www.cisa.gov/known-ex
 [{cisa_item["cveID"]}](https://nvd.nist.gov/vuln/detail/{cisa_item["cveID"]})
 
 ## Severity
-Severity: {cvss_severity} (CVSS {cvss_version})  **[{cvss_score}]**   
-Vector: {cvss_vector}   
+
+{cvss_severity} - CVSS v{cvss_version} {cvss_score} - ({cvss_vector})
+
+## Detailed description of the vulnerability
 
 {nvd_description}
 
-## Mitigation
-{cisa_item["cveID"]} is currently being exploited in the wild and is a high-priority vulnerability.
+## Reporter
+
+CISA
 '''
-        labels = ["Vulnerability", "CISA-Alert"]
+
+        labels = ["Vulnerability", "CISA-Alert", f"security-issue-severity::{cvss_severity}".lower()]
         return description, labels
 
     def create_github_issues(self, new_items):
-    """ create GitHub issues for new items """
-    log_message("Creating GitHub issues for CISA vulnerabilities")
-    
-    # Get the current month and year
-    current_month_year = datetime.now().strftime("%Y/%m")
-
-    for cisa_item in new_items:
-        description, labels = self.generate_description_and_labels(cisa_item, self.get_nvd_data(cisa_item["cveID"]))
+        """ create GitHub issues for new items """
+        log_message("Creating GitHub issues for CISA vulnerabilities")
         
-        # Modify the title to include the current month and year, followed by "Internal-CISA"
-        issue_title = f"{current_month_year} : Internal-CISA - {cisa_item['vendorProject']} {cisa_item['product']} - {cisa_item['cveID']}"
-        
-        # Create the issue with the modified title
-        issue = self.repo.create_issue(
-            title=issue_title,
-            body=description,
-            labels=labels,
-            milestone=self.milestone
-        )
+        # Get the current month and year
+        current_month_year = datetime.now().strftime("%Y/%m")
 
-        # Add comment, label, and close issue with delays
-        time.sleep(60)  # Wait for 1 minute
-        issue.create_comment("This vulnerability is not applicable to the product/application, so closing this issue.")
-        time.sleep(120)  # Wait for 2 minutes
-        issue.add_labels(Constants.LABEL_REMEDIATED)
-        time.sleep(180)  # Wait for 3 minutes
-        issue.edit(state="closed")
+        for cisa_item in new_items:
+            description, labels = self.generate_description_and_labels(cisa_item, self.get_nvd_data(cisa_item["cveID"]))
+            
+            # Modify the title to include the current month and year, followed by "Internal-CISA"
+            issue_title = f"{current_month_year} : Internal-CISA - {cisa_item['vendorProject']} {cisa_item['product']} - {cisa_item['cveID']}"
+            
+            # Create the issue with the modified title
+            issue = self.repo.create_issue(
+                title=issue_title,
+                body=description,
+                labels=labels
+            )
 
-    log_complete("GitHub issues creation completed")
+            # Wait for 1 minute, then add milestone
+            time.sleep(60)  # Wait for 1 minute
+            issue.edit(milestone=self.repo.get_milestone(Constants.MILESTONE_NAME))
 
+            # Add comment, label, and close issue with delays
+            time.sleep(60)  # Wait for 1 minute
+            issue.create_comment("This vulnerability is not applicable to the product/application, so closing this issue.")
+            time.sleep(120)  # Wait for 2 minutes
+            issue.add_labels(Constants.LABEL_REMEDIATED)
+            time.sleep(180)  # Wait for 3 minutes
+            issue.edit(state="closed")
+
+        log_complete("GitHub issues creation completed")
 
 if __name__ == "__main__":
     Main().main()
