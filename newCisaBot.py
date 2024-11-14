e6a03b9865a0ad973bbb2293f2d0813317fd06b0
Omer <136714975+ums91@users.noreply.github.com>
2024-11-14 20:01:37 +0530

    Update newCisaBot.py

--- newCisaBot.py
+++ newCisaBot.py
@@ -291,7 +291,9 @@ CISA
         # Filter out vulnerabilities that are already in the README
         new_vulnerabilities = []
         for item in new_items:
-            vulnerability_entry = f"- **{item['cveID']}**: ({item['vulnerabilityName']}) <br> Vendor Project: {item.get('title', f'{item.get('vendorProject', 'Unknown Vendor')} <br> Product: {item.get('product', 'Unknown Product')}')}"
+            vulnerability_entry = f"- **{item['cveID']}**: ({item['vulnerabilityName']}) 
+            Vendor Project: {item.get('title', f'{item.get('vendorProject', 'Unknown Vendor')}\n
+            Product: {item.get('product', 'Unknown Product')}')}"\n
             if vulnerability_entry not in readme_content:
                 new_vulnerabilities.append(vulnerability_entry)
 
